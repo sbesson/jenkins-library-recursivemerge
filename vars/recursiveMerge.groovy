e5715802011f9ea290c78a985c983ffee81a1383
jmoore <josh@glencoesoftware.com>
2019-05-30 11:29:17 +0100

    Cleanup the multiple version files

--- vars/recursiveMerge.groovy
+++ vars/recursiveMerge.groovy
@@ -5,21 +5,21 @@ def call(Map pipelineParams) {
     def buildInfraUrl = pipelineParams.get('buildInfraUrl', 'https://github.com/${buildInfraRepo}/build-infra/archive/${buildInfraBRanch}.tar.gz | tar -zxf -')
     def buildInfraPath = pipelineParams.get('buildInfraPath', 'build-infra-${buildInfraBranch}')
 
+    cleanWs()
+    copyArtifacts(projectName: pipelineParams.parentVersions, flatten: false,
+                    filter: pipelineParams.versionFile, target: 'build')
+
     // build is in .gitignore so we can use it as a temp dir
     sh 'mkdir -p build'
     sh 'cd build && curl -sfL ${buildInfraUrl} | tar -zxf -'
     sh 'virtualenv build/venv && build/venv/bin/pip install scc'
-
-    copyArtifacts(projectName: pipelineParams.parentVersions, flatten: false,
-                    filter: pipelineParams.versionFile, target: 'build')
 
     sh """
         export BASE_REPO=${pipelineParams.baseRepo}
 
         # Workaround for "unflattened" file, possibly due to matrix
         find build/ -path "${pipelineParams.versionFile}" -exec cp {} build/version.tsv \\;
-        test -e build/version.tsv
-        export VERSION_LOG=${env.WORKSPACE}/build/version.tsv
+        export VERSION_LOG=${pwd()}/build/version.tsv
 
         . build/venv/bin/activate
         if [ "${env.MERGE_PUSH_BRANCH}" != "null" ]; then
